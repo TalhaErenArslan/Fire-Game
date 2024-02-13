d0e6dc140666c41ffb3b253ebdc706a9fbcb4f2b
TalhaErenArslan <talhaeren.arslan01@gmail.com>
2024-02-13 13:27:52 +0300

    Finished

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Threading;
 
 namespace game
 {
@@ -27,37 +28,63 @@ namespace game
         Rectangle enemy = new Rectangle();
         Rectangle enemy1 = new Rectangle();
         Rectangle enemy2 = new Rectangle();
+        Rectangle enemy1l = new Rectangle();
         Rect kutu; //?
         int formW = 1200;  // Form 
         int formH = 600;
         int ballW = 40;
-        int ballH = 40;
+        int ballH = 50;
         private DateTime startTime;
         private DateTime endTime;
-        private TextBlock timeTextBlock;
+        private const int moveStep = 5;
+        private const int rotateStep = 2; // Dönme adımı
+        private bool moveRight = true;
+        private double rotationAngle = 0;
+
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void StartButton_Click_Level1(object sender, RoutedEventArgs e)
+        {
+            startTime = DateTime.Now;
+            Canvas1.Children.Clear();
             Canvas1.Children.Add(ball);
             Canvas1.Children.Add(enemy);
             Canvas1.Children.Add(enemy1);
             Canvas1.Children.Add(enemy2);
+            // Canvas1.Children.Add(enemy1l);
             Canvas1.Children.Add(door);
-
-        }
-        private void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            startTime = DateTime.Now;
-            Canvas1.Children.RemoveAt(0);
-            Canvas1.Children.RemoveAt(0);
             setBall();
             setDoor();
             setEnemy();
+            // setEnemy1L();
             setStaticEnemy();
             setStaticEnemy2();
             enemyMove();
+            // enemyMove1();
         }
+        private void StartButton_Click_Level2(object sender, RoutedEventArgs e)
+        {
+            startTime = DateTime.Now;
+            Canvas1.Children.Clear();
+            Canvas1.Children.Add(ball);
+            Canvas1.Children.Add(enemy);
+            Canvas1.Children.Add(enemy1);
+            Canvas1.Children.Add(enemy2);
+            Canvas1.Children.Add(enemy1l);
+            Canvas1.Children.Add(door);
+            setBall();
+            setDoor();
+            setEnemy();
+            setEnemy1L();
+            setStaticEnemy();
+            setStaticEnemy2();
+            enemyMove();
+            enemyMove1();
+        }
+
         private void setBall()
         {
             ball.Tag = "ball";
@@ -65,36 +92,51 @@ namespace game
             ball.Height = ballH;
             ball.Fill = new ImageBrush
             {
-                ImageSource = new BitmapImage(new Uri(@"https://www.pngmart.com/files/1/Fire-Flame-PNG-Clipart.png", UriKind.Absolute))
+                ImageSource = new BitmapImage(new Uri("images/fire.png", UriKind.Relative))
             };
             Canvas.SetLeft(ball, 1);
             Canvas.SetTop(ball, 450);
         }
         private void setDoor()
         {
-            door.Width = 150;
-            door.Height = 200;
+            door.Width = 400;
+            door.Height = 450;
             door.Tag = "door";
             door.Fill = new ImageBrush
             {
-                ImageSource = new BitmapImage(new Uri(@"file:///C:/Users/talha/Downloads/image-PhotoRoom.png-PhotoRoom%20(2).png", UriKind.Absolute))
+                ImageSource = new BitmapImage(new Uri("images/home.png", UriKind.Relative))
             };
-            Canvas.SetLeft(door, 880);
-            Canvas.SetTop(door, 300);
+            Canvas.SetLeft(door, 890);
+            Canvas.SetTop(door, 100);
         }
         private void setEnemy()
         {
-            enemy.Width = 80;
-            enemy.Height = 50;
+            enemy.Width = 20;
+            enemy.Height = 60;
             enemy.Tag = "enemy";
             enemy.Fill = new ImageBrush
             {
-                ImageSource = new BitmapImage(new Uri(@"file:///C:/Users/talha/Downloads/image-PhotoRoom.png-PhotoRoom%20(3).png", UriKind.Absolute))
+                ImageSource = new BitmapImage(new Uri("images/05water.png", UriKind.Relative))
             };
-            Canvas.SetLeft(enemy, 880);
+            Canvas.SetLeft(enemy, 920);
             Canvas.SetTop(enemy, 450);
 
         }
+
+        private void setEnemy1L()
+        {
+            enemy1l.Width = 20;
+            enemy1l.Height = 80;
+            enemy1l.Tag = "enemy";
+            enemy1l.Fill = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri("images/1water.png", UriKind.Relative))
+            };
+            Canvas.SetLeft(enemy1l, 920);
+            Canvas.SetTop(enemy1l, 20);
+
+        }
+
         private void setStaticEnemy()
         {
 
@@ -103,7 +145,7 @@ namespace game
             enemy1.Tag = "enemy";
             enemy1.Fill = new ImageBrush
             {
-                ImageSource = new BitmapImage(new Uri(@"file:///C:/Users/talha/Downloads/image-PhotoRoom.png-PhotoRoom%20(1).png", UriKind.Absolute))
+                ImageSource = new BitmapImage(new Uri("images/water.png", UriKind.Relative))
             };
             Canvas.SetLeft(enemy1, 400);
             Canvas.SetTop(enemy1, 450);
@@ -117,7 +159,7 @@ namespace game
             enemy2.Tag = "enemy";
             enemy2.Fill = new ImageBrush
             {
-                ImageSource = new BitmapImage(new Uri(@"file:///C:/Users/talha/Downloads/image-PhotoRoom.png-PhotoRoom%20(1).png", UriKind.Absolute))
+                ImageSource = new BitmapImage(new Uri("images/water.png", UriKind.Relative))
             };
             Canvas.SetLeft(enemy2, 700);
             Canvas.SetTop(enemy2, 450);
@@ -128,19 +170,28 @@ namespace game
             double y;
             while (true)
             {
+
                 Random rand = new Random();
                 int randomNumber1 = rand.Next(100, 170);
                 y = Canvas.GetLeft(enemy) - 10;
                 // if (x < 0) x = 0;
                 Canvas.SetLeft(enemy, y);
-                await Task.Delay(5);
+                // await Task.Delay(10);
 
                 if (Canvas.GetLeft(enemy) < -40)
                 {
-                    Canvas.SetLeft(enemy, 980);
+                    Canvas.SetLeft(enemy, 1200);
                     Canvas.SetTop(enemy, randomNumber1);
                 }
+                RotateTransform rotateTransform = new RotateTransform(rotationAngle);
+                enemy.RenderTransform = rotateTransform;
 
+                rotationAngle += rotateStep;
+                if (rotationAngle >= 360)
+                {
+                    rotationAngle = 0;
+                }
+                await Task.Delay(5);
                 kutu = new Rect(Canvas.GetLeft(ball), Canvas.GetTop(ball), ball.Width, ball.Height);
 
                 foreach (var x in Canvas1.Children.OfType<Rectangle>())
@@ -151,18 +202,81 @@ namespace game
                     {
                         if (kutu.IntersectsWith(hitBox))
                         {
-                            Canvas1.Background = new ImageBrush
+                            door.Fill = new ImageBrush
                             {
-                                ImageSource = new BitmapImage(new Uri(@"file:///C:/Users/talha/OneDrive/Masa%C3%BCst%C3%BC/atesss.png", UriKind.Absolute))
+                                ImageSource = new BitmapImage(new Uri("images/home_fire.png", UriKind.Relative))
                             };
                             endTime = DateTime.Now;
                             TimeSpan elapsedTime = endTime - startTime;
                             int seconds = (int)elapsedTime.TotalSeconds;
                             GameOver("Tebrikler, Ortalığı ateşe vermeyi başardın hemde" + " " + seconds.ToString() + " " + "saniyede yaptın");
+
                         }
                     }
 
                     if ((string)x.Tag == "enemy")
+                    {
+                        if (kutu.IntersectsWith(hitBox))
+                        {
+                            ball.Fill = Brushes.Gray;
+                            GameOver("Ahh Olamaz ! Üstüne su sıçradı ve söndün");
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void enemyMove1()
+        {
+            double y;
+            while (true)
+            {
+                await Task.Delay(10);
+                Random rand = new Random();
+                int randomNumber1 = rand.Next(100, 170);
+                y = Canvas.GetLeft(enemy1l) - 10;
+                // if (x < 0) x = 0;
+                Canvas.SetLeft(enemy1l, y);
+                // await Task.Delay(10);
+
+                if (Canvas.GetLeft(enemy1l) < -40)
+                {
+                    Canvas.SetLeft(enemy1l, 1200);
+                    Canvas.SetTop(enemy1l, randomNumber1);
+                }
+                RotateTransform rotateTransform = new RotateTransform(rotationAngle);
+                enemy1l.RenderTransform = rotateTransform;
+
+                rotationAngle += rotateStep;
+                if (rotationAngle >= 360)
+                {
+                    rotationAngle = 0;
+                }
+                await Task.Delay(10);
+                kutu = new Rect(Canvas.GetLeft(ball), Canvas.GetTop(ball), ball.Width, ball.Height);
+
+                foreach (var x in Canvas1.Children.OfType<Rectangle>())
+                {
+                    Rect hitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                    if ((string)x.Tag == "door")
+                    {
+                        if (kutu.IntersectsWith(hitBox))
+                        {
+                            door.Fill = new ImageBrush
+                            {
+                                ImageSource = new BitmapImage(new Uri("images/home_fire.png", UriKind.Relative))
+                            };
+                            endTime = DateTime.Now;
+                            TimeSpan elapsedTime = endTime - startTime;
+                            int seconds = (int)elapsedTime.TotalSeconds;
+                            GameOver("Tebrikler, Ortalığı ateşe vermeyi başardın hemde" + " " + seconds.ToString() + " " + "saniyede yaptın");
+
+                        }
+                    }
+
+                    if ((string)x.Tag == "enemy1l")
                     {
                         if (kutu.IntersectsWith(hitBox))
                         {
@@ -225,8 +339,8 @@ namespace game
         private void GameOver(string message)
         {
             MessageBox.Show(message, "Ortalığı ateşe ver!");
+            System.Windows.Application.Current.Shutdown();
             System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
-            Application.Current.Shutdown();
         }
 
     }
